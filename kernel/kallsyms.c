a2928f2a4e2ea734fd7351ae1ce739f39bbc7b77
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2020-04-06 07:46:24 +0000

    Revert "FROMGIT: kallsyms: unexport kallsyms_lookup_name() and kallsyms_on_each_symbol()"
    Our out of tree wlan_mt7668_sdio driver uses kallsyms_on_each_symbol:
    
        [    2.038929] wlan_mt7668_sdio: Unknown symbol kallsyms_on_each_symbol (err -2)
    
    As we can see, wlan_mt7668_sdio does not probe anymore because of commit
    8e37367a324263c8c1b31ab14030d6be27b0cc7e.
    
    This reverts commit 8e37367a324263c8c1b31ab14030d6be27b0cc7e.
    
    Bug: RITA-214
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -199,6 +199,7 @@ unsigned long kallsyms_lookup_name(const char *name)
 	}
 	return module_kallsyms_lookup_name(name);
 }
+EXPORT_SYMBOL_GPL(kallsyms_lookup_name);
 
 int kallsyms_on_each_symbol(int (*fn)(void *, const char *, struct module *,
 				      unsigned long),
@@ -217,6 +218,7 @@ int kallsyms_on_each_symbol(int (*fn)(void *, const char *, struct module *,
 	}
 	return module_kallsyms_on_each_symbol(fn, data);
 }
+EXPORT_SYMBOL_GPL(kallsyms_on_each_symbol);
 
 static unsigned long get_symbol_pos(unsigned long addr,
 				    unsigned long *symbolsize,
