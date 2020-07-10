cd7b836d51953e1e903dc8cf8f55eea197bb1c23
Julien STEPHAN <jstephan@baylibre.com>
2020-07-10 12:55:24 +0200

    rpmsg: apu_rpmsg: correctly free rproc resource
    we need to call rproc_put when freing resources to decrement refcount
    after using rproc_get_by_phandle
    
    Signed-off-by: Julien STEPHAN <jstephan@baylibre.com>

--- drivers/rpmsg/apu_rpmsg.c
+++ drivers/rpmsg/apu_rpmsg.c
@@ -563,6 +563,7 @@ free_minor_ida:
 	ida_simple_remove(&rpmsg_minor_ida, MINOR(dev->devt));
 free_apu:
 	put_device(dev);
+	rproc_put(apu->rproc);
 	kfree(apu);
 
 	return ret;
@@ -577,6 +578,7 @@ static void apu_rpmsg_remove(struct rpmsg_device *rpdev)
 
 	device_del(&apu->dev);
 	put_device(&apu->dev);
+	rproc_put(apu->rproc);
 	kfree(apu);
 }
 
