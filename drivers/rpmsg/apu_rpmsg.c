f3968c4bdb8c6634ba6a63471dd69902f1a73605
Alexandre Bailon <abailon@baylibre.com>
2020-10-09 09:48:02 +0200

    BL_INTERNAL:rpmsg: apu_rpmsg: Fix a oops happening while rproc is stopping
    The commit 8352d19cfb4cdd5e5f2362e712a842e2ea5578f2
    has introduced a regession in the driver.
    On the third stop operation of remoteproc, the driver hangs or oops.
    The reason is that we call rproc_put whereas we don't increase its
    reference counter anymore, which cause some unexecpted behaviors.
    Don't use rproc_put since this not required anymore.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

--- drivers/rpmsg/apu_rpmsg.c
+++ drivers/rpmsg/apu_rpmsg.c
@@ -695,7 +695,6 @@ free_minor_ida:
 	ida_simple_remove(&rpmsg_minor_ida, MINOR(dev->devt));
 free_apu:
 	put_device(dev);
-	rproc_put(apu->rproc);
 	devm_kfree(&rpdev->dev, apu);
 
 	return ret;
@@ -710,7 +709,6 @@ static void apu_rpmsg_remove(struct rpmsg_device *rpdev)
 
 	device_del(&apu->dev);
 	put_device(&apu->dev);
-	rproc_put(apu->rproc);
 	devm_kfree(&rpdev->dev, apu);
 }
 
