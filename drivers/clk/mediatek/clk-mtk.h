f4c562026c3c8e64d265ea66fba85e26f633d072
Fabien Parent <fparent@baylibre.com>
2020-11-06 19:34:58 +0100

    BL_INTERNAL:clk: mediatek: add mt8365 clock driver
    Add clk driver for MT8365
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

--- drivers/clk/mediatek/clk-mtk.h
+++ drivers/clk/mediatek/clk-mtk.h
@@ -181,6 +181,7 @@ struct mtk_clk_divider {
 	unsigned long flags;
 
 	u32 div_reg;
+	u32 div_reg_fixup;
 	unsigned char div_shift;
 	unsigned char div_width;
 	unsigned char clk_divider_flags;
@@ -247,5 +248,81 @@ void mtk_register_reset_controller(struct device_node *np,
 
 void mtk_register_reset_controller_set_clr(struct device_node *np,
 	unsigned int num_regs, int regofs);
+
+struct clk_fixup_div {
+	int id;
+	const char *name;
+	const char *parent_name;
+
+	void __iomem *reg_fixup;
+	const struct clk_div_table *clk_div_table;
+
+	struct clk_divider divider;
+	const struct clk_ops *ops;
+
+};
+
+static inline struct clk_fixup_div *to_clk_fixup_div(struct clk_hw *hw)
+{
+	struct clk_divider *divider = to_clk_divider(hw);
+
+	return container_of(divider, struct clk_fixup_div, divider);
+}
+
+struct clk *mtk_clk_fixup_divider(
+		const char *name,
+		const char *parent,
+		unsigned long flags,
+		void __iomem *reg,
+		void __iomem *reg_fixup,
+		u8 shift,
+		u8 width,
+		u8 clk_divider_flags,
+		spinlock_t *lock);
+
+void mtk_clk_register_fixup_dividers(const struct mtk_clk_divider *mcds,
+			int num, void __iomem *base, spinlock_t *lock,
+				struct clk_onecell_data *clk_data);
+
+#define CLR_SET_UPD_FLAGS(_id, _name, _parents, _mux_ofs, _mux_set_ofs,\
+			_mux_clr_ofs, _shift, _width, _gate,		\
+			_upd_ofs, _upd, _flags, _ops) {			\
+		.id = _id,						\
+		.name = _name,						\
+		.mux_ofs = _mux_ofs,					\
+		.set_ofs = _mux_set_ofs,				\
+		.clr_ofs = _mux_clr_ofs,				\
+		.upd_ofs = _upd_ofs,					\
+		.mux_shift = _shift,					\
+		.mux_width = _width,					\
+		.gate_shift = _gate,					\
+		.upd_shift = _upd,					\
+		.parent_names = _parents,				\
+		.num_parents = ARRAY_SIZE(_parents),			\
+		.flags = _flags,					\
+	}
+
+#define MUX_CLR_SET_UPD_FLAGS(_id, _name, _parents, _mux_ofs, _mux_set_ofs,\
+			_mux_clr_ofs, _shift, _width, _gate,		\
+			_upd_ofs, _upd, _flags)			\
+		CLR_SET_UPD_FLAGS(_id, _name, _parents, _mux_ofs,	\
+			_mux_set_ofs, _mux_clr_ofs, _shift, _width,	\
+			_gate, _upd_ofs, _upd, _flags,			\
+			mtk_mux_clr_set_upd_ops)
+
+#define MUX_CLR_SET_UPD(_id, _name, _parents, _mux_ofs, _mux_set_ofs,	\
+			_mux_clr_ofs, _shift, _width, _gate,		\
+			_upd_ofs, _upd)				\
+		MUX_CLR_SET_UPD_FLAGS(_id, _name, _parents, _mux_ofs,	\
+			_mux_set_ofs, _mux_clr_ofs, _shift, _width,	\
+			_gate, _upd_ofs, _upd, CLK_SET_RATE_PARENT)
+
+#define MUX_UPD(_id, _name, _parents, _mux_ofs, _shift, _width, _gate,	\
+			_upd_ofs, _upd)				\
+		CLR_SET_UPD_FLAGS(_id, _name, _parents, _mux_ofs,	\
+			INVALID_OFS, INVALID_OFS, _shift, _width,	\
+			_gate, _upd_ofs, _upd, CLK_SET_RATE_PARENT,	\
+			mtk_mux_upd_ops)
+
 
 #endif /* __DRV_CLK_MTK_H */
