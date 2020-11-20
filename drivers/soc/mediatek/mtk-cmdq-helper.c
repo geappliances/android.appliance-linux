465a0dd9397805a7b430574e0c2c9374e0de60f8
Fabien Parent <fparent@baylibre.com>
2020-11-20 21:21:59 +0100

    UPSTREAM_OR_FROMLIST: Revert "soc: mediatek: cmdq: export finalize function"
    This reverts commit bc66b0362b1f07c2705b950d9c3d9ee838ed4962.

--- drivers/soc/mediatek/mtk-cmdq-helper.c
+++ drivers/soc/mediatek/mtk-cmdq-helper.c
@@ -300,7 +300,7 @@ int cmdq_pkt_assign(struct cmdq_pkt *pkt, u16 reg_idx, u32 value)
 }
 EXPORT_SYMBOL(cmdq_pkt_assign);
 
-int cmdq_pkt_finalize(struct cmdq_pkt *pkt)
+static int cmdq_pkt_finalize(struct cmdq_pkt *pkt)
 {
 	struct cmdq_instruction inst = { {0} };
 	int err;
@@ -319,7 +319,6 @@ int cmdq_pkt_finalize(struct cmdq_pkt *pkt)
 
 	return err;
 }
-EXPORT_SYMBOL(cmdq_pkt_finalize);
 
 static void cmdq_pkt_flush_async_cb(struct cmdq_cb_data data)
 {
@@ -353,6 +352,10 @@ int cmdq_pkt_flush_async(struct cmdq_pkt *pkt, cmdq_async_flush_cb cb,
 	int err;
 	unsigned long flags = 0;
 	struct cmdq_client *client = (struct cmdq_client *)pkt->cl;
+
+	err = cmdq_pkt_finalize(pkt);
+	if (err < 0)
+		return err;
 
 	pkt->cb.cb = cb;
 	pkt->cb.data = data;
