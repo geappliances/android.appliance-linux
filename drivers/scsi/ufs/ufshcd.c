401c7fce0e621527713e2e751a3a19291009ee77
Can Guo <cang@codeaurora.org>
2021-01-13 19:13:27 -0800

    BL_INTERNAL:scsi: ufs: Fix a possible NULL pointer issue
    During system resume/suspend, hba could be NULL. In this case, do not touch
    eh_sem.
    
    Fixes: 88a92d6ae4fe ("scsi: ufs: Serialize eh_work with system PM events and async scan")
    Link: https://lore.kernel.org/r/1610594010-7254-2-git-send-email-cang@codeaurora.org
    Acked-by: Stanley Chu <stanley.chu@mediatek.com>
    Signed-off-by: Can Guo <cang@codeaurora.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit fb7afe24ba1b7e27483be7d2ac3ed002e67eecd5)
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -100,6 +100,8 @@
 		       16, 4, buf, __len, false);                        \
 } while (0)
 
+static bool early_suspend;
+
 int ufshcd_dump_regs(struct ufs_hba *hba, size_t offset, size_t len,
 		     const char *prefix)
 {
@@ -9088,8 +9090,14 @@ int ufshcd_system_suspend(struct ufs_hba *hba)
 	int ret = 0;
 	ktime_t start = ktime_get();
 
-	down(&hba->host_sem);
-	if (!hba || !hba->is_powered)
+	if (!hba) {
+		early_suspend = true;
+		return 0;
+	}
+
+	down(&hba->eh_sem);
+
+	if (!hba->is_powered)
 		return 0;
 
 	cancel_delayed_work_sync(&hba->rpm_dev_flush_recheck_work);
@@ -9141,9 +9149,12 @@ int ufshcd_system_resume(struct ufs_hba *hba)
 	int ret = 0;
 	ktime_t start = ktime_get();
 
-	if (!hba) {
-		up(&hba->host_sem);
+	if (!hba)
 		return -EINVAL;
+
+	if (unlikely(early_suspend)) {
+		early_suspend = false;
+		down(&hba->eh_sem);
 	}
 
 	if (!hba->is_powered || pm_runtime_suspended(hba->dev))
