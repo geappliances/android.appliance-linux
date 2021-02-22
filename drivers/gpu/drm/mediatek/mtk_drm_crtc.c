8fff2299b0061f33b491e6e2a149a7c18f57527e
Amjad Ouled-Ameur <aouledameur@baylibre.com>
2021-02-22 13:33:50 +0000

    BL_INTERNAL:drm/mediatek: fix iommu address translation faults
    This fixes the following iommu faults which were seen during kernel boot:
    mtk-iommu 10203000.m4u: fault type=0x5 iova=0x0 pa=0x0 larb=0 port=3 layer=0 read
    mtk-iommu 10203000.m4u: fault type=0x5 iova=0x0 pa=0x0 larb=0 port=3 layer=0 read
    mtk-iommu 10203000.m4u: fault type=0x5 iova=0x0 pa=0x0 larb=0 port=3 layer=0 read
    mtk-iommu 10203000.m4u: fault type=0x5 iova=0x0 pa=0x0 larb=0 port=3 layer=0 read
    mtk-iommu 10203000.m4u: fault type=0x5 iova=0x0 pa=0x0 larb=0 port=3 layer=0 read
    mtk-iommu 10203000.m4u: fault type=0x5 iova=0x0 pa=0x0 larb=0 port=3 layer=0 read
    mtk-iommu 10203000.m4u: fault type=0x5 iova=0x0 pa=0x0 larb=0 port=3 layer=0 read
    mtk-iommu 10203000.m4u: fault type=0x5 iova=0x0 pa=0x0 larb=0 port=3 layer=0 read
    mtk-iommu 10203000.m4u: fault type=0x5 iova=0x0 pa=0x0 larb=0 port=3 layer=0 read
    mtk-iommu 10203000.m4u: fault type=0x5 iova=0x0 pa=0x0 larb=0 port=3 layer=0 read
    
    type=0x5 corresponds to the following faults:
        - Translation fault
        - Invalid physical address fault
    
    In fact, (port=3, larb=0) corresponds to disp_rdma.
    
    Moreover, disp_rdma can operate in two modes:
        - memory mode: reading data directly from DRAM through DMA.
        - direct link mode: no data reading is performed.
    
    MediaTek products use this component to read data from DRAM, hence
    disp_rdma operates in memory mode. To perform a reading, we pass the
    address of the first pixel we wish to read, along with the width of the
    source frame. The address of the first pixel should be a DMA address, more
    specifically the address of the DMA buffer, that MUST be allocated prior
    the rdma read.
    
    The problem is that a NULL dma address is passed to disp_rdma, hence
    mtk-iommu fails because there is no mapping for this address. This
    happens because, while initializing disp_rdma, the DMA address that is
    passed to it, is not updated with the DMA buffer that has been allocated
    by DRM (by calling mtk_drm_gem_create()).
    
    To fix this issue, mtk_plane_atomic_update() can be used to update the DMA
    address with the currently allocated DMA buffer address. This call also
    performs validity checks on the DMA buffer before doing any updates.
    
    Use mtk_plane_atomic_update() to update DMA addr before configuring ddp
    components including disp_rdma.
    
    However, this patches only iommu faults seen at boot time with
    iova=pa=0. Other faults occurs after kernel boot, suspend/resume and
    shutdown/reboot, those faults have iova!=0 and pa=0 which probably means
    that disp_rdma tries to read from a DMA buffer that has been unmapped
    (that is why iova is not NULL and seems to be valid, while pa = 0 because
    there is no longer a mapping to it). Upcoming patches will address this
    particular issue.
    
    RITA-401
    
    Signed-off-by: Amjad Ouled-Ameur <aouledameur@baylibre.com>
    [mkorpershoek: ported to 5.10]
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- drivers/gpu/drm/mediatek/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mtk_drm_crtc.c
@@ -320,6 +320,9 @@ static int mtk_crtc_ddp_hw_init(struct mtk_drm_crtc *mtk_crtc)
 		unsigned int local_layer;
 
 		plane_state = to_mtk_plane_state(plane->state);
+
+		plane->helper_private->atomic_update(plane, plane->state);
+
 		comp = mtk_drm_ddp_comp_for_plane(crtc, plane, &local_layer);
 		if (comp)
 			mtk_ddp_comp_layer_config(comp, local_layer,
