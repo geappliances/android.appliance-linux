7b5fbf8468d1ea2016799724bbd49adcdd900405
Phi-Bang Nguyen <pnguyen@baylibre.com>
2021-03-15 18:15:39 +0100

    media: platform: seninf: Don't hardcode the source pad index
    In order to support multiple sensors, source pads should be extendable.
    When setting format, the format should be propagated from the sink pad
    to its corresponding source pad using the actual routing configuration.
    
    Signed-off-by: Phi-Bang Nguyen <pnguyen@baylibre.com>

--- drivers/media/platform/mtk-isp/seninf/mtk_seninf.c
+++ drivers/media/platform/mtk-isp/seninf/mtk_seninf.c
@@ -30,7 +30,6 @@
 #define SENINF_HS_TRAIL_PARAMETER	0x8
 
 #define SENINF_NUM_INPUTS		4
-#define SENINF_PAD_SOURCE		4
 #define SENINF_NUM_PADS			8
 
 #define SENINF_DEFAULT_WIDTH		1920
@@ -819,16 +818,15 @@ static int seninf_set_fmt(struct v4l2_subdev *sd,
 	fmt->format = *format;
 
 	/*
-	 * Propagate the format to the source pad. The internal routing
-	 * configuration is controlled through the active sink link.
+	 * Propagate the format to the corresponding source pad.
 	 *
-	 * TODO: We should disallow setting formats on the source pad
+	 * TODO (?): We should disallow setting formats on the source pad
 	 * completely, as the SENINF can't perform any processing. This would
 	 * however break usage of the test pattern generator, as there would be
 	 * no way to configure formats at all when no active input is selected.
 	 */
-	if (priv->active_input && fmt->pad == priv->active_input->port) {
-		format = seninf_get_pad_format(priv, cfg, SENINF_PAD_SOURCE,
+	if (priv->inputs[fmt->pad].source_pad) {
+		format = seninf_get_pad_format(priv, cfg, priv->inputs[fmt->pad].source_pad,
 					       fmt->which);
 		*format = fmt->format;
 	}
