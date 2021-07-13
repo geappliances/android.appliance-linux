geappliances/android.appliance-linux

commit 01023042ace02cbfed838d49f80d3cd836cf3c2b

iommu: mtk_iommu: Add MT8365 support
Add support for the IOMMU on MT8365.

Signed-off-by: Fabien Parent <fparent@baylibre.com>

drivers/iommu/mtk_iommu.c
@@ -857,11 +857,18 @@ static const struct mtk_iommu_plat_data mt8183_data = {
 	.larbid_remap = {0, 4, 5, 6, 7, 2, 3, 1},
 };
 
+static const struct mtk_iommu_plat_data mt8365_data = {
+	.m4u_plat     = M4U_MT8365,
+	.reset_axi = true,
+	.larbid_remap = {{0}, {1}, {2}, {3}, {4}, {5}}, /* Linear mapping. */
+};
+
 static const struct of_device_id mtk_iommu_of_ids[] = {
 	{ .compatible = "mediatek,mt2712-m4u", .data = &mt2712_data},
 	{ .compatible = "mediatek,mt8167-m4u", .data = &mt8167_data},
 	{ .compatible = "mediatek,mt8173-m4u", .data = &mt8173_data},
 	{ .compatible = "mediatek,mt8183-m4u", .data = &mt8183_data},
+	{ .compatible = "mediatek,mt8365-m4u", .data = &mt8365_data},
 	{}
 };
 

drivers/iommu/mtk_iommu.h
@@ -33,6 +33,7 @@ enum mtk_iommu_plat {
 	M4U_MT8167,
 	M4U_MT8173,
 	M4U_MT8183,
+	M4U_MT8365,
 };
 
 struct mtk_iommu_plat_data {

commit 3449af710e616f4eea788cc9335b7e83a5468e7b

iommu/mediatek: Fix -Wbraced-scalar-init for MT8365
When building with -Werror, we have the following build error:

drivers/iommu/mtk_iommu.c:863:19: error: braces around scalar initializer [-Werror,-Wbraced-scalar-init]
      .larbid_remap = {{0}, {1}, {2}, {3}, {4}, {5}}, /* Linear mapping. */

Fix it by removing the un-needed braces.

Fixes: 01023042ace0 ("iommu: mtk_iommu: Add MT8365 support")
Suggested-by: Guillaume La Roque <glaroque@baylibre.com>
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

drivers/iommu/mtk_iommu.c
@@ -860,7 +860,7 @@ static const struct mtk_iommu_plat_data mt8183_data = {
 static const struct mtk_iommu_plat_data mt8365_data = {
 	.m4u_plat     = M4U_MT8365,
 	.reset_axi = true,
-	.larbid_remap = {{0}, {1}, {2}, {3}, {4}, {5}}, /* Linear mapping. */
+	.larbid_remap = {0, 1, 2, 3, 4, 5}, /* Linear mapping. */
 };
 
 static const struct of_device_id mtk_iommu_of_ids[] = {