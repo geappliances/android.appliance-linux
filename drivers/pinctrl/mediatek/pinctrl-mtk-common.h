1327ddefd2ce0d9f5db9d01c76576b5172ac5b6b
Fabien Parent <fparent@baylibre.com>
2021-11-07 18:39:57 +0100

    UPSTREAM_OR_FROMLIST: Revert "pinctrl: mediatek: common: add quirk for broken set/clr modes"
    This reverts commit 6b822cd0232f4c0a5b0ea600bbd828a0834c3f4f.

--- drivers/pinctrl/mediatek/pinctrl-mtk-common.h
+++ drivers/pinctrl/mediatek/pinctrl-mtk-common.h
@@ -22,8 +22,6 @@
 
 #define MTK_PINCTRL_NOT_SUPPORT	(0xffff)
 
-#define MTK_PINCTRL_MODE_SET_CLR_BROKEN   BIT(0)
-
 struct mtk_desc_function {
 	const char *name;
 	unsigned char muxval;
@@ -259,7 +257,6 @@ struct mtk_pinctrl_devdata {
 	unsigned int mode_mask;
 	unsigned int mode_per_reg;
 	unsigned int mode_shf;
-	unsigned long quirks;
 };
 
 struct mtk_pinctrl {
