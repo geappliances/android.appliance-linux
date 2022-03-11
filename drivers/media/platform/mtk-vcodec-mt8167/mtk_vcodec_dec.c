ea21e378f671a522eee81a91a9b90449261b1196
Macross Chen <macross.chen@mediatek.com>
2022-03-11 10:12:39 +0800

    BL_INTERNAL: media: mtk-vcodec-mt8167: vdec: fix mpeg1/2/4 framesize step
    set mpeg1/2 framesize with step 8
    set mpeg4 framesize with step 1
    
    Signed-off-by: Macross Chen <macross.chen@mediatek.com>

--- drivers/media/platform/mtk-vcodec-mt8167/mtk_vcodec_dec.c
+++ drivers/media/platform/mtk-vcodec-mt8167/mtk_vcodec_dec.c
@@ -106,18 +106,18 @@ static const struct mtk_codec_framesizes mtk_vdec_framesizes[] = {
 	},
 	{
 		.fourcc	= V4L2_PIX_FMT_MPEG1,
-		.stepwise = {  MTK_VDEC_MIN_W, MTK_VDEC_MAX_W, 16,
-				MTK_VDEC_MIN_H, MTK_VDEC_MAX_H, 16 },
+		.stepwise = {  MTK_VDEC_MIN_W, MTK_VDEC_MAX_W, 8,
+				MTK_VDEC_MIN_H, MTK_VDEC_MAX_H, 8 },
 	},
 	{
 		.fourcc	= V4L2_PIX_FMT_MPEG2,
-		.stepwise = {  MTK_VDEC_MIN_W, MTK_VDEC_MAX_W, 16,
-				MTK_VDEC_MIN_H, MTK_VDEC_MAX_H, 16 },
+		.stepwise = {  MTK_VDEC_MIN_W, MTK_VDEC_MAX_W, 8,
+				MTK_VDEC_MIN_H, MTK_VDEC_MAX_H, 8 },
 	},
 	{
 		.fourcc	= V4L2_PIX_FMT_MPEG4,
-		.stepwise = {  MTK_VDEC_MIN_W, MTK_VDEC_MAX_W, 16,
-				MTK_VDEC_MIN_H, MTK_VDEC_MAX_H, 16 },
+		.stepwise = {  MTK_VDEC_MIN_W, MTK_VDEC_MAX_W, 1,
+				MTK_VDEC_MIN_H, MTK_VDEC_MAX_H, 1 },
 	},
 };
 
