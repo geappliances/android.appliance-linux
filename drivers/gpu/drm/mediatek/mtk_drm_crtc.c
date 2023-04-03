ff32222a6522e786154a3f28368203cc1f2528cf
Vitor Sato Eschholz <vsatoes@baylibre.com>
2023-04-03 10:11:44 +0000

    BL_ITNERNAL: drm: mediatek: mtk_drm_crtc: fix lvds initialization
    Before using LVDS encoder, LVDSPLL needs to be set, and pixel clock
    frequency derived from VPLL needs to be present. Place the setup
    of pixel clock before ddp component start.
    
    It fixes commit 4289b8fe00 drm/mediatek: Add LVDS support in MTK DRM.
    
    Signed-off-by: Andrew Perepech <andrew.perepech@mediatek.com>
    Signed-off-by: Vitor Sato Eschholz <vsatoes@baylibre.com>
    (cherry picked from commit 60b5d2b4fbd70925a1da7d39735d51072025da01)

--- drivers/gpu/drm/mediatek/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mtk_drm_crtc.c
@@ -308,11 +308,11 @@ static int mtk_crtc_ddp_hw_init(struct mtk_drm_crtc *mtk_crtc)
 		if (i == 1)
 			mtk_ddp_comp_bgclr_in_on(comp);
 
-		mtk_ddp_comp_config(comp, width, height, vrefresh, bpc, NULL);
-		mtk_ddp_comp_start(comp);
-
 		if (mtk_crtc->ddp_comp[i]->id == DDP_COMPONENT_LVDS)
 			mtk_mmsys_ddp_lvds_sys_cfg_lvds(mtk_crtc->mmsys_dev);
+
+		mtk_ddp_comp_config(comp, width, height, vrefresh, bpc, NULL);
+		mtk_ddp_comp_start(comp);
 	}
 
 	/* Initially configure all planes */
